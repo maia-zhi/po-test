8ea6bd37e7f9451e456a0873f60f09a753efc4f1
maia <maiag8724@gmail.com>
2023-07-04 16:36:12 +0800

    Add files via upload

---
+++ type.js
@@ -0,0 +1,7 @@
+let typeEffect = new Typed(".multiText", {
+    strings: ["Web Designer!", "Front-End Beginner!", "Graphic Designer!"],
+    loop: true,
+    typeSpeed: 80,
+    backSpeed:60,
+    backDelay: 1500,
+})
